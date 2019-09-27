cec9c91bc45cfdd1cd8d639104895f6e814aad10
seal-mbo <51327970+seal-mbo@users.noreply.github.com>
2019-09-27 12:41:06 +0200

    feat: PLS-431
    - Removed roboter
     - Installed latest dev stack
     - Updated scripts
     - Updated GitHub Pull Request template
     - Updated CircleCI config
     - Updated appveyor config
     - Updated dependencies
     - Updated eslintrc
     - Added dependabot config
     - Added `package-lock.json` to git
     - Fixed linting

--- test/odataMongoTest.js
+++ test/odataMongoTest.js
@@ -33,104 +33,9 @@ suite('odataMongo', () => {
     });
 
     test('provides req.mongo.', (done) => {
-      request(app).get('/foo').end((err, res) => {
-        assert.that(err).is.null();
-        assert.that(res.status).is.equalTo(200);
-        assert.that(res.body).is.equalTo({
-          query: {},
-          queryOptions: {}
-        });
-        done();
-      });
-    });
-
-    test('parses $top.', (done) => {
-      request(app).get('/foo?$top=5').end((err, res) => {
-        assert.that(err).is.null();
-        assert.that(res.status).is.equalTo(200);
-        assert.that(res.body).is.equalTo({
-          query: {},
-          queryOptions: {
-            limit: 5
-          },
-          inlineCount: false
-        });
-        done();
-      });
-    });
-
-    test('parses $skip.', (done) => {
-      request(app).get('/foo?$skip=100').end((err, res) => {
-        assert.that(err).is.null();
-        assert.that(res.status).is.equalTo(200);
-        assert.that(res.body).is.equalTo({
-          query: {},
-          queryOptions: {
-            skip: 100
-          },
-          inlineCount: false
-        });
-        done();
-      });
-    });
-
-    test('parses $orderby.', (done) => {
-      request(app).get('/foo?$orderby=bar').end((err, res) => {
-        assert.that(err).is.null();
-        assert.that(res.status).is.equalTo(200);
-        assert.that(res.body).is.equalTo({
-          query: {},
-          queryOptions: {
-            sort: [
-              [
-                'bar',
-                'asc'
-              ]
-            ]
-          },
-          inlineCount: false
-        });
-        done();
-      });
-    });
-
-    test('parses $inlinecount=none.', (done) => {
-      request(app).get('/foo?$inlinecount=none').end((err, res) => {
-        assert.that(err).is.null();
-        assert.that(res.status).is.equalTo(200);
-        assert.that(res.body).is.equalTo({
-          query: {},
-          queryOptions: {},
-          inlineCount: false
-        });
-        done();
-      });
-    });
-
-    test('parses $inlinecount=allpages.', (done) => {
-      request(app).get('/foo?$inlinecount=allpages').end((err, res) => {
-        assert.that(err).is.null();
-        assert.that(res.status).is.equalTo(200);
-        assert.that(res.body).is.equalTo({
-          query: {},
-          queryOptions: {},
-          inlineCount: true
-        });
-        done();
-      });
-    });
-
-    test('returns a 400 on an unknown keyword.', (done) => {
-      request(app).get('/foo?$bar=baz').end((err, res) => {
-        assert.that(err).is.null();
-        assert.that(res.status).is.equalTo(400);
-        done();
-      });
-    });
-
-    suite('parses $select', () => {
-      test('does not set queryOptions.select if no fields are given.', (done) => {
-        request(app).get('/foo').end((err, res) => {
+      request(app)
+        .get('/foo')
+        .end((err, res) => {
           assert.that(err).is.null();
           assert.that(res.status).is.equalTo(200);
           assert.that(res.body).is.equalTo({
@@ -139,127 +44,245 @@ suite('odataMongo', () => {
           });
           done();
         });
-      });
+    });
 
-      test('sets queryOptions.select with the given fields.', (done) => {
-        request(app).get('/foo?$select=firstName, lastName').end((err, res) => {
+    test('parses $top.', (done) => {
+      request(app)
+        .get('/foo?$top=5')
+        .end((err, res) => {
           assert.that(err).is.null();
           assert.that(res.status).is.equalTo(200);
           assert.that(res.body).is.equalTo({
             query: {},
             queryOptions: {
-              fields: {
-                firstName: 1,
-                lastName: 1
-              }
+              limit: 5
             },
             inlineCount: false
           });
           done();
         });
+    });
+
+    test('parses $skip.', (done) => {
+      request(app)
+        .get('/foo?$skip=100')
+        .end((err, res) => {
+          assert.that(err).is.null();
+          assert.that(res.status).is.equalTo(200);
+          assert.that(res.body).is.equalTo({
+            query: {},
+            queryOptions: {
+              skip: 100
+            },
+            inlineCount: false
+          });
+          done();
+        });
+    });
+
+    test('parses $orderby.', (done) => {
+      request(app)
+        .get('/foo?$orderby=bar')
+        .end((err, res) => {
+          assert.that(err).is.null();
+          assert.that(res.status).is.equalTo(200);
+          assert.that(res.body).is.equalTo({
+            query: {},
+            queryOptions: {
+              sort: [['bar', 'asc']]
+            },
+            inlineCount: false
+          });
+          done();
+        });
+    });
+
+    test('parses $inlinecount=none.', (done) => {
+      request(app)
+        .get('/foo?$inlinecount=none')
+        .end((err, res) => {
+          assert.that(err).is.null();
+          assert.that(res.status).is.equalTo(200);
+          assert.that(res.body).is.equalTo({
+            query: {},
+            queryOptions: {},
+            inlineCount: false
+          });
+          done();
+        });
+    });
+
+    test('parses $inlinecount=allpages.', (done) => {
+      request(app)
+        .get('/foo?$inlinecount=allpages')
+        .end((err, res) => {
+          assert.that(err).is.null();
+          assert.that(res.status).is.equalTo(200);
+          assert.that(res.body).is.equalTo({
+            query: {},
+            queryOptions: {},
+            inlineCount: true
+          });
+          done();
+        });
+    });
+
+    test('returns a 400 on an unknown keyword.', (done) => {
+      request(app)
+        .get('/foo?$bar=baz')
+        .end((err, res) => {
+          assert.that(err).is.null();
+          assert.that(res.status).is.equalTo(400);
+          done();
+        });
+    });
+
+    suite('parses $select', () => {
+      test('does not set queryOptions.select if no fields are given.', (done) => {
+        request(app)
+          .get('/foo')
+          .end((err, res) => {
+            assert.that(err).is.null();
+            assert.that(res.status).is.equalTo(200);
+            assert.that(res.body).is.equalTo({
+              query: {},
+              queryOptions: {}
+            });
+            done();
+          });
+      });
+
+      test('sets queryOptions.select with the given fields.', (done) => {
+        request(app)
+          .get('/foo?$select=firstName, lastName')
+          .end((err, res) => {
+            assert.that(err).is.null();
+            assert.that(res.status).is.equalTo(200);
+            assert.that(res.body).is.equalTo({
+              query: {},
+              queryOptions: {
+                fields: {
+                  firstName: 1,
+                  lastName: 1
+                }
+              },
+              inlineCount: false
+            });
+            done();
+          });
       });
     });
 
     suite('parses $filter', () => {
       test('with an equal expression.', (done) => {
-        request(app).get('/foo?$filter=User eq \'Jane Doe\'').end((err, res) => {
-          assert.that(err).is.null();
-          assert.that(res.status).is.equalTo(200);
-          assert.that(res.body).is.equalTo({
-            query: {
-              User: 'Jane Doe'
-            },
-            queryOptions: {},
-            inlineCount: false
+        request(app)
+          .get("/foo?$filter=User eq 'Jane Doe'")
+          .end((err, res) => {
+            assert.that(err).is.null();
+            assert.that(res.status).is.equalTo(200);
+            assert.that(res.body).is.equalTo({
+              query: {
+                User: 'Jane Doe'
+              },
+              queryOptions: {},
+              inlineCount: false
+            });
+            done();
           });
-          done();
-        });
       });
 
       test('with support for unicode.', (done) => {
-        request(app).get(encodeURI('/foo?$filter=User eq \'라인에ᄅ\'')).end((err, res) => {
-          assert.that(err).is.null();
-          assert.that(res.status).is.equalTo(200);
-          assert.that(res.body).is.equalTo({
-            query: {
-
-              // Korean for R-a-in-e-r :-)
-              User: '라인에ᄅ'
-            },
-            queryOptions: {},
-            inlineCount: false
+        request(app)
+          .get(encodeURI("/foo?$filter=User eq '라인에ᄅ'"))
+          .end((err, res) => {
+            assert.that(err).is.null();
+            assert.that(res.status).is.equalTo(200);
+            assert.that(res.body).is.equalTo({
+              query: {
+                // Korean for R-a-in-e-r :-)
+                User: '라인에ᄅ'
+              },
+              queryOptions: {},
+              inlineCount: false
+            });
+            done();
           });
-          done();
-        });
       });
 
       test('with multiple expressions and parentheses.', (done) => {
-        request(app).get('/foo?$filter=(User eq \'Jane Doe\' and Id eq 23) or Id gt 42').end((err, res) => {
-          assert.that(err).is.null();
-          assert.that(res.status).is.equalTo(200);
-          assert.that(res.body).is.equalTo({
-            query: {
-              $or: [
-                { $and: [{ User: 'Jane Doe' }, { Id: 23 }] },
-                { Id: { $gt: 42 } }
-              ]
-            },
-            queryOptions: {},
-            inlineCount: false
+        request(app)
+          .get("/foo?$filter=(User eq 'Jane Doe' and Id eq 23) or Id gt 42")
+          .end((err, res) => {
+            assert.that(err).is.null();
+            assert.that(res.status).is.equalTo(200);
+            assert.that(res.body).is.equalTo({
+              query: {
+                $or: [{ $and: [{ User: 'Jane Doe' }, { Id: 23 }] }, { Id: { $gt: 42 } }]
+              },
+              queryOptions: {},
+              inlineCount: false
+            });
+            done();
           });
-          done();
-        });
       });
 
       test('with nested properties.', (done) => {
-        request(app).get('/foo?$filter=User/FirstName eq \'Jane\'').end((err, res) => {
-          assert.that(err).is.null();
-          assert.that(res.status).is.equalTo(200);
-          assert.that(res.body).is.equalTo({
-            query: {
-              'User.FirstName': 'Jane'
-            },
-            queryOptions: {},
-            inlineCount: false
+        request(app)
+          .get("/foo?$filter=User/FirstName eq 'Jane'")
+          .end((err, res) => {
+            assert.that(err).is.null();
+            assert.that(res.status).is.equalTo(200);
+            assert.that(res.body).is.equalTo({
+              query: {
+                'User.FirstName': 'Jane'
+              },
+              queryOptions: {},
+              inlineCount: false
+            });
+            done();
           });
-          done();
-        });
       });
 
       test('with multiply nested properties.', (done) => {
-        request(app).get('/foo?$filter=User/Name/First eq \'Jane\'').end((err, res) => {
-          assert.that(err).is.null();
-          assert.that(res.status).is.equalTo(200);
-          assert.that(res.body).is.equalTo({
-            query: {
-              'User.Name.First': 'Jane'
-            },
-            queryOptions: {},
-            inlineCount: false
+        request(app)
+          .get("/foo?$filter=User/Name/First eq 'Jane'")
+          .end((err, res) => {
+            assert.that(err).is.null();
+            assert.that(res.status).is.equalTo(200);
+            assert.that(res.body).is.equalTo({
+              query: {
+                'User.Name.First': 'Jane'
+              },
+              queryOptions: {},
+              inlineCount: false
+            });
+            done();
           });
-          done();
-        });
       });
 
       test('with functions.', (done) => {
-        request(app).get('/foo?$filter=startswith(User, \'Jane\') eq true').end((err, res) => {
-          assert.that(err).is.null();
-          assert.that(res.status).is.equalTo(200);
-          assert.that(res.body).is.equalTo({
-            query: { User: /^Jane/i.toString() },
-            queryOptions: {},
-            inlineCount: false
+        request(app)
+          .get("/foo?$filter=startswith(User, 'Jane') eq true")
+          .end((err, res) => {
+            assert.that(err).is.null();
+            assert.that(res.status).is.equalTo(200);
+            assert.that(res.body).is.equalTo({
+              query: { User: /^Jane/i.toString() },
+              queryOptions: {},
+              inlineCount: false
+            });
+            done();
           });
-          done();
-        });
       });
 
       test('returns a 400 on an invalid filter expression.', (done) => {
-        request(app).get('/foo?$filter=User xx').end((err, res) => {
-          assert.that(err).is.null();
-          assert.that(res.status).is.equalTo(400);
-          done();
-        });
+        request(app)
+          .get('/foo?$filter=User xx')
+          .end((err, res) => {
+            assert.that(err).is.null();
+            assert.that(res.status).is.equalTo(400);
+            done();
+          });
       });
     });
   });
